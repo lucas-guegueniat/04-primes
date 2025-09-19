c53d1ac53eb497277763b74f6d2602b369987002
Lucas GUEGUENIAT <lucas.guegueniat@edu.esiee.fr>
2025-09-19 10:04:43 +0000

    Complétion du code pour les nombres premiers

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-from math import sqrt
-
+"""
+Ce module vérifie qu'un nombre est premier dans la fonction secondaire "isprime()" 
+et teste cette dernière dans la fonction principale "main()" pour plusieurs nombres.
+"""
 #### Fonction secondaire
 
 
 def isprime(p):
+    """
+    Vérifie que p est un nombre premier.
+    """
 
     # votre code ici
-
-    pass
+    for i in range(1,p):
+        while i != 1 and p%i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
+    """
+    Vérifie le bon fonctionnement de la fonction "isprime()" 
+    """
 
     # vos appels à la fonction secondaire ici
 
-    for n in range(100):
+    for n in range(2,100):
         if isprime(n):
             print(n, end=", ")
 
